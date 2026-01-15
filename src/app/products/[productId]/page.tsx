52da8893b78ab272335fbcafef51f162cf72ee67
Minh Nguyen <minhkhac1002@gmail.com>
2026-01-15 11:27:34 +0700

    dynamic router

---
+++ src/app/products/[productId]/page.tsx
@@ -0,0 +1,6 @@
+export default async function ProductDetail({params} : {params: Promise<{productId: string}> }) {
+    const {productId} = await params
+    return (
+        <h1>Product Detail {productId}</h1>
+    )
+}
