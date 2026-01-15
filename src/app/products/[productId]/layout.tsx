ac6146f9e317a313afe886f96e3a15bfcdf9c90a
Minh Nguyen <minhkhac1002@gmail.com>
2026-01-15 14:30:13 +0700

    nested layouts

---
+++ src/app/products/[productId]/layout.tsx
@@ -0,0 +1,12 @@
+export default function ProductDetailLayout({
+    children
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <>
+            <h2>Product detail layout</h2>
+            {children}
+        </>
+    )
+}
