f0d0222cfe1e8a127be66bd65f527258e88b583a
Minh Nguyen <minhkhac1002@gmail.com>
2026-01-15 11:33:58 +0700

    nested dynamic routes

---
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -0,0 +1,9 @@
+export default async function ProductDetail(
+    {params} : 
+    {params: Promise<{productId: string, reviewId: string}> }
+    ) {
+    const {productId, reviewId} = await params
+    return (
+        <h1>Review {reviewId} for Product {productId} </h1>
+    )
+}
