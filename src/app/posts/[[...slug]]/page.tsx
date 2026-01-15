8ad5ab327db9e6bb5f8818a0ac9f519243e14bcc
Minh Nguyen <minhkhac1002@gmail.com>
2026-01-15 11:52:36 +0700

    catch all segment

---
+++ src/app/posts/[[...slug]]/page.tsx
@@ -0,0 +1,14 @@
+export default async function Posts({
+    params 
+}: {
+        params: Promise<({slug: string[]})>
+    }
+) {
+    const {slug} = await params
+    if(slug?.length ===2 ) {
+        return <h1>Feature {slug[0]} - Concept {slug[1]} </h1>
+    } else if (slug?.length === 1){
+        return <h1>Feature {slug[0]}</h1>
+    }
+    return <h1>Posts home page</h1>
+}
